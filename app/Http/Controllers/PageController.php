54c016dfd3cfeb3f0fbd5b4ba4c4eefa63c96f1d
diazrizqi <diazrizqi05@gmail.com>
2024-06-10 17:52:20 +0700

    file pertama

---
+++ app/Http/Controllers/PageController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PageController extends Controller
+{
+    public function index(){
+        return 'ini halaman index menggunakan controller';
+    }
+
+    public function request(){
+        return 'ini halaman request menggunakan controller';
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,59 @@
+<?php
+
+use App\Http\Controllers\PageController;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('hello', function () {
+    return 'ini laravel saya';
+});
+
+Route::get('tampil/{nilai}', function ($nilai) {
+    return 'nilai saya adalah '. $nilai;
+});
+
+Route::get('penjumlahan/{nilai1}/{nilai2}', function ($nilai1,$nilai2) {
+    return 'hasil penjumlahan adalah '. $nilai1 + $nilai2;
+});
+
+Route::get('pengurangan/{nilai1}/{nilai2}', function ($nilai1,$nilai2) {
+    return 'hasil pengurangan adalah '. $nilai1 - $nilai2;
+});
+
+Route::get('perkalian/{nilai1}/{nilai2}', function ($nilai1,$nilai2) {
+    return 'hasil perkalian adalah '. $nilai1 * $nilai2;
+});
+
+Route::get('pembagian/{nilai1}/{nilai2}', function ($nilai1,$nilai2) {
+    return 'hasil pembagian adalah '. $nilai1 / $nilai2;
+});
+
+Route::match(['get', 'post'], '/user/profile', function () {
+    return 'match';
+});
+
+Route::get('pagecontroller', [PageController::class, 'index']);
+Route::get('pagecontrollerrequest', [PageController::class, 'request']);
+
+Route::get('request', function (Request $request) {
+    return $request;
+});
+// Route::get('users/{id}', function ($id) {});
+// Route::post('users/{id}', function ($id) {});
+
+// Route::put('users/{id}', function ($id) {});
+// Route::delete('users/{id}', function ($id) {});
