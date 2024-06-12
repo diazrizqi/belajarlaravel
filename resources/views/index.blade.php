23a1676ec91b29b117ea747cf61dd9d1789808d2
diazrizqi <diazrizqi05@gmail.com>
2024-06-12 10:06:05 +0700

    Tugas Mandiri Kedua

---
+++ resources/views/index.blade.php
@@ -0,0 +1,5 @@
+@extends('template')
+@section('main')
+    <h1>Ini halaman Index</h1>
+    
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -66,3 +66,19 @@ Route::get('table', function () {
     $data = ['meja','kursi'];
     return view('table', compact('data'));
 });
+
+Route::get('login', function () {
+    return view('login');
+});
+
+Route::get('register', function () {
+    return view('register');
+});
+
+Route::get('index', function () {
+    return view('index');
+});
+
+Route::get('navbar', function () {
+    return view('navbar');
+});
